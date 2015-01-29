38683d01247c6c693567226696fe266fc804bf2f
gborchers2 <greg.borchers@comcast.net>
2015-01-28 18:02:33 -0800

    adding the homework back

---
+++ gregBorchers/deque-template.js
@@ -0,0 +1,52 @@
+// Problem 2a): build a deque factory
+//-------
+
+// The factory itself:
+function makeDeque(values) {
+	// ...
+	// return an instance object
+}
+
+// The factory's instance methods:
+makeDeque.length = function() {
+	//...
+}
+
+makeDeque.top = function() {
+	//...
+}
+
+makeDeque.bottom = function() {
+	//...
+}
+
+makeDeque.pop = function() {
+	//...
+}
+
+makeDeque.push = function(val) {
+	//...
+}
+
+makeDeque.shift = function() {
+	//...
+}
+
+makeDeque.unshift = function(val) {
+	//...
+}
+
+makeDeque.cut = function() {
+	//...
+}
+
+makeDeque.map = function(convertValFn) {
+	//...
+}
+
+makeDeque.sort = function(compareValsFn) {
+	//...
+}
+
+// Feel free to write tests for your code!
+
